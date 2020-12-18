f01d6ec18f8840f19669a7d041dee3fb7527d253
Lukman Nur Hakim <luckymanlukman19@gmail.com>
2020-12-18 19:57:22 +0700

    first commit

---
+++ 01-Fundamentals/Part-1/script.js
@@ -0,0 +1,30 @@
+/*
+////////////////////////////////////
+// Linking a JavaScript File
+let js = "amazing";
+console.log(40 + 19 + 21 - 11);
+////////////////////////////////////
+// Values and Variables
+console.log("Lukman");
+console.log(19);
+
+let firstName = "Thaira";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let lukman_thaira = "LT";
+let $function = 20;
+
+let person = "lukman";
+let PI = 3.1415;
+*/
+let myFirstJob = "Coder";
+let myCurrentJob = "Waiterw";
+
+let job1 = "coder";
+let job2 = "waiters";
+
+console.log(myCurrentJob);
