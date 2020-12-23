02fb955cf60ec63ecb0ba96b323bf3a93ecda1eb
Lukman Nur Hakim <luckymanlukman19@gmail.com>
2020-12-23 12:58:47 +0700

    Update

--- 01-Fundamentals/Part-2/script.js
+++ 01-Fundamentals/Part-2/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let hasDriversLicensi = false;
 const passTest = true;
 
@@ -8,3 +8,189 @@ if(hasDriversLicensi) console.log('I can Drive XD');
 
 // const interface = 'Audio';
 // const private = 123;
+
+///////////////////// 
+// Function
+function logger(){
+    console.log('My name is Lukman');
+}
+
+//  calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(manggos, oranges){
+    const juice = `Juice with ${manggos} manggo and ${oranges} orange`;
+    return juice;
+}
+
+const juiceManggo = fruitProcessor(2, 3);
+console.log(juiceManggo);
+const juiceOrange = fruitProcessor(3, 6);
+console.log(juiceOrange);
+
+const num = Number('19');
+
+// function declaration
+function calcAge1(birthYear){
+    return 2025 - birthYear;
+}
+const age1 = calcAge1(2001);
+
+// function expression
+const calcAge2 = function(birthYear){
+    return 2024 - birthYear;
+}
+const age2 = calcAge2(2001);
+
+console.log(age1,age2);
+
+// arrow function
+const calcAge3 = birthYear => 2025 - birthYear;
+const age3 = calcAge3(2001);
+console.log(age3);
+
+const yearUntilMarriage = (birthYear, firstName) => {
+    const age = 2025 - birthYear;
+    const merriage = 27 - age;
+    return `${firstName} merriage in ${merriage}`;
+}
+console.log(yearUntilMarriage(2001, 'Lukman'));
+console.log(yearUntilMarriage(2005, 'Fatimah'));
+
+// calling function
+function cutFruitPieces(fruit){
+    return fruit * 5
+}
+function fruitProcessor(manggos, oranges){
+    const manggoPieces = cutFruitPieces(manggos);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${manggoPieces} piece of manggo and ${orangePieces} piece of orange`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+const calcAge = function(birthYear){
+    return 2025 - birthYear;
+}
+const yearUntilMarriage = function(birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const merriage = 27 - age;
+    if(merriage > 0){
+        console.log(`${firstName} merriage in ${merriage} years.`);
+        return merriage;
+    } else {
+        console.log(`${firstName} has already merriage.`);
+        return -1;
+    }
+}
+
+console.log(yearUntilMarriage(2001, 'Lukman'));
+console.log(yearUntilMarriage(1994, 'Rahmat'));
+
+//// Array
+const friend1 = 'Mugni';
+const friend2 = 'Dicki';
+const friend3 = 'Rahmat';
+
+const friends = ['Mugni', 'Dicki', 'Rahmat'];
+console.log(friends);
+console.log(friends[0]);
+console.log(friends[2]);
+
+const y = new Array(2000, 2001, 2003);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Martha';
+console.log(friends);
+
+const firstName = 'Lukman';
+const lukman = [firstName, 'Nur Hakim', 2027 - 2001, friends];
+console.log(lukman);
+console.log(lukman.lenth);
+
+// Exercise
+const calcAge = function(birthYear){
+    return 2027 - birthYear;
+}
+const year = [1999, 2000, 2003, 2005, 2009];
+
+console.log(calcAge(year[0]));
+console.log(calcAge(year[2]));
+console.log(calcAge(year[year.length - 1]));
+
+const years = new Array(calcAge(year[0]), calcAge(year[2]), calcAge(year[year.length - 1]));
+// const years = [calcAge(year[0]), calcAge(year[2]), calcAge(year[year.length - 1])];
+console.log(years);
+
+const friends = ['Mugni', 'Dicki', 'Rahmat'];
+
+// Add Array
+const newLength = friends.push('Martha');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('Yusuf');
+console.log(friends);
+
+// Remove Array
+friends.pop(); // last
+const pooped = friends.pop();
+console.log(pooped);
+console.log(friends);
+
+friends.shift(); // beginning
+console.log(friends);
+
+friends.push(23);
+console.log(friends.indexOf('Mugni'));
+console.log(friends.indexOf('Tiara'));
+console.log(friends.includes(23));
+
+if(friends.includes('Mugni')){
+    console.log('You have a friend called Mugni.');
+}
+
+const lukmanArray = [
+    'Lukman',
+    'Nur Hakim',
+    2025 - 2001,
+    'Programmer',
+    ['Mugni', 'Dicki', 'Rahmat']
+];
+*/
+//// Object
+const lukman = {
+    firstName: 'Lukman',
+    lastName: 'Nur Hakim',
+    age: 2025 - 2001,
+    job: 'Programmer',
+    friends: ['Mugni', 'Dicki', 'Rahmat']
+};
+
+console.log(lukman);
+console.log(lukman.firstName);
+console.log(lukman['firstName']);
+
+const nameKey = 'Name';
+console.log(lukman['first' + nameKey]);
+console.log(lukman['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Lukman? Choose betwwen firstName, lastName, age, job, friends.');
+if(lukman[interestedIn]){
+    console.log(lukman[interestedIn]);
+} else {
+    console.log('Wrong!! Choose betwwen firstName, lastName, age, job, friends.')
+}
+
+lukman.location = 'Indonesia';
+lukman['instagram'] = '@luckymn_';
+console.log(lukman);
+
+// Challenge
+// Lukman has 3 friends and his best friend is called mugni
+
+
+console.log(`${lukman.firstName} has ${lukman.friends.length} friends and his best friend is called ${lukman.friends[0]}`);
--- 01-Fundamentals/coding challenge/script.js
+++ 01-Fundamentals/coding challenge/script.js
@@ -129,3 +129,68 @@ HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 
 
 
+//////////// Part 2
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+*/
+
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// //// Data 1
+// let avgDolhins = calcAverage(44, 23, 71);
+// let avgKoalas = calcAverage(65, 54, 49);
+// console.log(avgDolhins, avgKoalas);
+
+// const checkWinner = function(avgDolhins, avgKoalas){
+//     if(avgDolhins >= 2 * avgKoalas){
+//         console.log(`Dolphins win (${avgDolhins} vs ${avgKoalas})`);
+//     } else if(avgKoalas >= 2 * avgDolhins) {
+//         console.log(`Koalas win (${avgKoalas} vs ${avgDolhins})`);
+//     } else {
+//         console.log(`No teams win...`);
+//     }
+// }
+// checkWinner(avgDolhins, avgKoalas);
+// //// Data 2
+// avgDolhins = calcAverage(85, 54, 41);
+// avgKoalas = calcAverage(23, 34, 27);
+// console.log(avgDolhins, avgKoalas);
+// checkWinner(avgDolhins, avgKoalas);
+
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+ */
+
+// const calcTip = function(x){
+//     return x >= 50 && x <= 300 ? x * 0.15 : x * 0.2;
+// }
+
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(bills, tips, totals);
